8bc67dd7f35dd684e410b8fdb1df14560b95d702
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-07 16:35:05 -0800

    Pass 4th test

--- test/02-by-val-by-ref.test.js
+++ test/02-by-val-by-ref.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { 
     appendTheEnd,
     appendFirstToLast,
@@ -39,7 +40,7 @@ test('It should append by modifying the original', (assert) => {
 });
 
 
-skip('It should add a property to an object', (assert) => {
+test('It should add a property to an object', (assert) => {
     const a = { fullName: 'Octavia Butler' };
     addBirthYearProperty(a, 1947);
 
